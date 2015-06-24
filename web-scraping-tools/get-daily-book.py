6d55781efc75b1b989848972fbed9ab8084eaa6e
Paul Baker <paul.baker@overstock.com>
2015-06-23 19:02:43 -0600

    Adding new scraping file

---
+++ web-scraping-tools/get-daily-book.py
@@ -0,0 +1,15 @@
+author__ = 'paulbaker'
+'''
+get the packt book of the day
+'''
+
+import requests
+from lxml import html
+import re
+
+'''
+def login_to_packt()
+
+def load_promotional_page(page_url='https://www.packtpub.com/packt/offers/free-learning'):
+  pass
+'''
